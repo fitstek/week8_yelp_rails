b68400908dd1609a91ac3e7a75ae7a9508a22b4d
Fitsum <fitsum.teklehaimanot@gmail.com>
2014-07-10 23:00:57 +0100

    added a form, edit and update

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,29 @@ class RestaurantsController < ApplicationController
 	def index
 		@restaurants = Restaurant.all
 	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		Restaurant.create(params[:restaurant].permit(:name))
+		redirect_to '/restaurants'
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.update(params[:restaurant].permit(:name))
+		redirect_to '/restaurants'
+	end
+
+	private
+
+	# def restaurant_params
+	# 	params.require(:restaurant).permit(:name)
+	# end
 end
